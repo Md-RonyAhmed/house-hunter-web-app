f71ab257f4940bd78491f84db037d7808ddd5792
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-07-19 02:19:13 +0600

    [DONE] house module

--- Server/house-hunter-api-server/dist/app.js
+++ Server/house-hunter-api-server/dist/app.js
@@ -9,6 +9,10 @@ const express_1 = __importDefault(require('express'));
 const cors_1 = __importDefault(require('cors'));
 const http_status_1 = __importDefault(require('http-status'));
 const cookie_parser_1 = __importDefault(require('cookie-parser'));
+const globalErrorHandler_1 = __importDefault(
+  require('./app/middlewares/globalErrorHandler')
+);
+const index_1 = __importDefault(require('../src/app/routes/index'));
 //create app
 const app = (0, express_1.default)();
 // use cors
@@ -20,13 +24,13 @@ app.use((0, cookie_parser_1.default)());
 // url parser
 app.use(express_1.default.urlencoded({ extended: true }));
 //application route
-// app.use('/api/v1', routes);
+app.use('/api/v1', index_1.default);
 //home route
 app.get('/', (req, res) => {
   res.send('Welcome to our application');
 });
 //global error handler
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler_1.default);
 //handle not found route
 app.use((req, res, next) => {
   res.status(http_status_1.default.NOT_FOUND).json({
